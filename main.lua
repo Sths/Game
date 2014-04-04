5de9131edba1473e21e02db18d03a1465aa81255
Sths <stevenhtx1993@163.com>
2014-04-05 01:14:51 +0800

    第一次提交
    目前完成了按钮。

---
+++ main.lua
@@ -0,0 +1,70 @@
+-- Requires
+require "src\\button"
+
+-- Variables
+count = 0
+currentTime = 0
+mouse_x = 0
+mouse_y = 0
+mouse_button = "1"
+
+ll_game_scene = "title"
+
+ll_button_title = {}
+
+ll_mouse_buttonPressed = nil
+ll_mouse_buttonFocus = nil
+
+function love.draw()
+	if (ll_game_scene == "title") then
+		love.graphics.print("Passing Time : " .. tostring(count), 0, 0)
+		love.graphics.print(mouse_x .. ", " .. mouse_y .. ", " .. mouse_button, 100, 500)
+		--love.graphics.draw(image, love.mouse.getX(), love.mouse.getY(), 0, 100 / image:getWidth(), 75 / image:getHeight())
+		ll_button_title["start_game"]:draw()
+	end
+end
+
+function love.mousepressed(x, y, button)
+	mouse_x = x
+	mouse_y = y
+	mouse_button = button
+	if button == "l" then
+		if (ll_button_title["start_game"]:contains(x, y)) then
+			ll_button_title["start_game"]:onPress(x, y)
+			ll_mouse_buttonPressed = ll_button_title["start_game"]
+		end
+	end
+end
+
+function love.mousereleased(x, y)
+	if (ll_mouse_buttonPressed) then
+		ll_mouse_buttonPressed:onRelease()
+	end
+end
+
+function love.update(dt)
+	currentTime = currentTime + dt;
+	if (currentTime >= 1) then
+		currentTime = currentTime - 1;
+		count = count + 1
+	end
+	if ll_mouse_buttonFocus then
+		ll_mouse_buttonFocus:offFocus()
+	end
+	if (ll_button_title["start_game"]:contains(love.mouse.getX(), love.mouse.getY())) then
+		ll_button_title["start_game"]:onFocus(love.mouse.getX(), love.mouse.getY())
+		ll_mouse_buttonFocus = ll_button_title["start_game"]
+	end
+end
+-- TODO
+function love.load()
+
+	source = love.audio.newSource("1.mp3", "stream")
+	love.audio.play(source)
+
+	image = love.graphics.newImage("3.jpg")
+	love.window.setTitle("Secret Base")
+
+	ll_button_title["start_game"] = Button:new{deltax = 10, deltay = 20, width = 200, height = 150}
+	ll_button_title["start_game"]:loadImage("res/buttons/start_game/", ".png")
+end
---
+++ src/button.lua
@@ -0,0 +1,63 @@
+Button = {
+	deltax = 0,
+	deltay = 0,
+	width = 0,
+	height = 0,
+	images = {},
+	beFocused = false,
+	bePressed = false
+}
+
+function Button:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function Button:contains(x, y)
+	local x_lb = self.deltax
+	local x_ub = self.deltax + self.width
+	local y_lb = self.deltay
+	local y_ub = self.deltay + self.height
+	if x >= x_lb and x < x_ub and y >= y_lb and y < y_ub then
+		return true
+	end
+	return false
+end
+
+function Button:onPress(x, y)
+	self.bePressed = true
+end
+
+function Button:onRelease(x, y)
+	self.bePressed = false
+end
+
+function Button:onFocus(x, y)
+	self.beFocused = true;
+end
+
+function Button:offFocus(x, y)
+	self.beFocused = false;
+end
+
+function Button:loadImage(path, type)
+	self.images['normal'] = love.graphics.newImage(path .. "normal" .. type)
+	self.images['pressed'] = love.graphics.newImage(path .. "pressed" .. type)
+	self.images['focused'] = love.graphics.newImage(path .. "focused" .. type)
+end
+
+function Button:draw()
+	local image_to_draw;
+	if self.bePressed then
+		image_to_draw = self.images["pressed"]
+	else
+		if self.beFocused then
+			image_to_draw = self.images["focused"]
+		else
+			image_to_draw = self.images["normal"]
+		end
+	end
+	love.graphics.draw(image_to_draw, self.deltax, self.deltay, 0,  self.width / image_to_draw:getWidth(), self.height / image_to_draw:getHeight())
+end
