28bbe56b01d02194b73d543e344990b2aff2f3b9
Sths <stevenhtx1993@163.com>
2014-04-08 20:03:10 +0800

    标题页面修改
    1.修改了窗口分辨率
    2.修改了背景图片

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+	t.window.width = 1120
+	t.window.height = 630
+end
--- main.lua
+++ main.lua
@@ -11,6 +11,7 @@ mouse_button = "1"
 ll_game_scene = "title"
 
 ll_button_title = {}
+ll_image_title = {}
 
 ll_mouse_buttonPressed = nil
 ll_mouse_buttonFocus = nil
@@ -21,6 +22,7 @@ function love.draw()
 		--love.graphics.print(mouse_x .. ", " .. mouse_y .. ", " .. mouse_button, 100, 500)
 		--love.graphics.draw(image, love.mouse.getX(), love.mouse.getY(), 0, 100 / image:getWidth(), 75 / image:getHeight())
 		ll_button_title["start_game"]:draw()
+		love.graphics.draw(ll_image_title["background"], 0, 0);
 	end
 end
 
@@ -48,6 +50,10 @@ function love.update(dt)
 		currentTime = currentTime - 1;
 		count = count + 1
 	end
+
+	-- Mouse 
+
+	-- Mouse focus
 	if ll_mouse_buttonFocus then
 		ll_mouse_buttonFocus:offFocus()
 	end
@@ -61,9 +67,10 @@ function love.load()
 
 	source = love.audio.newSource("res/chores/1.mp3", "stream")
 	love.audio.play(source)
-
 	image = love.graphics.newImage("res/chores/3.jpg")
-	love.window.setTitle("Secret Base")
+	love.window.setTitle("Long life v.0.0.0000")
+
+	ll_image_title["background"] = love.graphics.newImage("res/backgrounds/title.png")
 
 	ll_button_title["start_game"] = Button:new{deltax = 10, deltay = 20, width = 200, height = 150}
 	ll_button_title["start_game"]:loadImage("res/buttons/start_game/", ".png")
